029b9e4f09cfc0d7199704ee1d2d124b0f83188c
nikolays <sidorovkolya91@gmail.com>
2023-01-18 14:38:10 +0300

    решение задачи1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,26 @@
 ﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int InputInt(string message)
+{
+  Console.WriteLine(message + " > ");
+  string? inputValue = Console.ReadLine();
+  int result = Convert.ToInt32(inputValue);
+  return result;
+}
+
+bool ValidateNumberRank2(int number)
+{
+  if (number > 99 && number < 1000)
+  {
+    return true;
+  }
+  System.Console.WriteLine("Число не трехзначное.");
+  return false;
+}
+
+int number = InputInt("Введите трехзначное число");
+if (ValidateNumberRank2(number))
+{
+  int Rank = number % 100/10;
+  System.Console.WriteLine($"Вторая цифра числа {number} равна {Rank}");
+}
