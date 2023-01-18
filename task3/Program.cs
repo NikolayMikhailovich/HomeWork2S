c10ecbfe4f7836c4d1cd09c7fd97990072155219
nikolays <sidorovkolya91@gmail.com>
2023-01-18 17:38:06 +0300

    Задаяа3

--- task3/Program.cs
+++ task3/Program.cs
@@ -4,15 +4,20 @@
 Console.WriteLine("введите номер дня недели");
 string dayofweek = Console.ReadLine();
 int numberday = Convert.ToInt32(dayofweek);
+
 if (numberday < 8 && numberday > 0)
 {
   string[] weeks = new string[7] { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье" };
-  System.Console.WriteLine(weeks[numberday - 1]);
-
+  Console.WriteLine(weeks[numberday - 1]);
 }
-else System.Console.WriteLine("Это не день недели");
+else Console.WriteLine("Это не день недели");
+
 if (5 < numberday && numberday < 8)
 {
-  System.Console.WriteLine("День является выходным");
+  Console.WriteLine("День является выходным");
 }
-
+else System.Console.WriteLine("День будний");
+if (numberday < 1 && numberday > 7)
+{
+  Console.WriteLine("Это не день недели");
+}
