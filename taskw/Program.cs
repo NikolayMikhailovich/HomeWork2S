fe9337f0394d48e2b97d3e1d1438a4084eda7aee
nikolays <sidorovkolya91@gmail.com>
2023-01-20 14:52:43 +0300

    Программа которая показывает вторую цифру числа или сообщает что второй цифры нет

---
+++ taskw/Program.cs
@@ -0,0 +1,14 @@
+﻿//Программа которая показывает вторую цифру числа или сообщает что второй цифры нет.
+
+Console.Write("Введите число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 1)
+{
+  Console.WriteLine("Вторая цифра -> " + anyNumberText[1]);
+}
+else
+{
+  Console.WriteLine("-> Второй цифры нет");
+}
+
